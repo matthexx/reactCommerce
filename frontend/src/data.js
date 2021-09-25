3af761f07d22b3b8b703ed366dd28bf06676512e
matthew <matthew_imadiyi@yahoo.com>
2021-09-25 12:02:36 +0100

    frontend ui templates

---
+++ frontend/src/data.js
@@ -0,0 +1,95 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'African Men Native',
+            category:'Traditional attire',
+            image: './images/p1.jpg',
+            price: 200,
+            brand: 'Agoro',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Sleek classic attire'
+        },
+        {
+            _id:'2',
+            name:'African Men Native Gold',
+            category:'Traditional attire',
+            image: './images/p2.jpg',
+            price: 400,
+            brand: 'Yomi Casual',
+            rating: 5,
+            numReviews: 20,
+            description: 'Classic attire'
+        },
+        {
+            _id:'3',
+            name:'South south Native',
+            category:'Senator attire',
+            image: './images/p3.jpg',
+            price: 800,
+            brand: 'Agoro',
+            rating: 5,
+            numReviews: 8,
+            description: 'Soft sleek classic attire'
+        },
+        {
+            _id:'4',
+            name:'African Men Native',
+            category:'Traditional attire',
+            image: './images/p1.jpg',
+            price: 400,
+            brand: 'Agoro',
+            rating: 3.5,
+            numReviews: 4,
+            description: 'Classic attire'
+        },
+        {
+            _id:'5',
+            name:'Native',
+            category:'Ankara',
+            image: './images/p5.jpg',
+            price: 600,
+            brand: 'Agoro',
+            rating: 3.5,
+            numReviews: 30,
+            description: 'Sleek ankara attire'
+        },
+        {
+            _id:'6',
+            name:'African Men Native',
+            category:'Traditional attire',
+            image: './images/p6.jpg',
+            price: 200,
+            brand: 'Yomi Casual',
+            rating: 4.5,
+            numReviews: 4,
+            description: 'Sleek classic attire'
+        },
+        {
+            _id:'7',
+            name:'African Men Native',
+            category:'Traditional attire',
+            image: './images/p7.jpg',
+            price: 1200,
+            brand: 'Agoro',
+            rating: 5,
+            numReviews: 15,
+            description: 'Classic attire'
+        },
+        {
+            _id:'8',
+            name:'African Men Native',
+            category:'Traditional attire',
+            image: './images/p1.jpg',
+            price: 200,
+            brand: '7n8',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Sleek classic attire'
+        }
+
+    ]
+}
+
+export default data;
